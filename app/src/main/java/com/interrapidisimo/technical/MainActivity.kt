78869d47c44cc84bba8b62bb59390f7b5c50de26
ingridmarcelasantosibarra <marcesntos@gmail.com>
2026-03-04 09:44:15 -0500

    Initial commit - Proyecto Android base

---
+++ app/src/main/java/com/interrapidisimo/technical/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.interrapidisimo.technical
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.interrapidisimo.technical.ui.theme.TechnicalTestInterrapidisimoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TechnicalTestInterrapidisimoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TechnicalTestInterrapidisimoTheme {
+        Greeting("Android")
+    }
+}
